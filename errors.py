f37970ea5e741fa747aac24348b0c288cc49bfa0
ferreira <jvitordiasferreira@gmail.com>
2026-03-23 20:40:01 -0300

    errors handling

---
+++ errors.py
@@ -0,0 +1,50 @@
+#basic syntax
+"""
+try:
+    #code that may cause error
+except:
+    #code that runs if error occurs
+"""
+
+'''
+try:
+    num =  int(input("Enter a number: "))
+    print(10/num)
+except:
+    print("Oops! Something went wrong.")
+'''
+
+'''
+#cathing specific errors
+try:
+    num = int(input("Enter a number: "))
+    print(10/num)
+except ValueError:
+    print("Oops! Something went wrong.")
+except ZeroDivisionError:
+    print("Oops! You can't divide by zero.")
+'''
+
+'''
+#using else
+try:
+    num = int(input("Enter a number: "))
+except ValueError:
+    print("Oops! Something went wrong.")
+except ZeroDivisionError:
+    print("Oops! You can't divide by zero.")
+else:
+    print("Result: ", 10/num)
+    #else runs when no errors happens
+'''
+
+#using finally
+try:
+    num = int(input("Enter a number: "))
+except ValueError:
+    print("Oops! Something went wrong.")
+except ZeroDivisionError:
+    print("Oops! You can't divide by zero.")
+finally:
+    print("Result: ", 10/num)
+
